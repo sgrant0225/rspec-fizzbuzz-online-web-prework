cdbb451d4b8a319c850af8efb8c1b3f85c0700b5
Sharnell Grant <sharnellgrant@gmail.com>
2019-05-24 11:21:52 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
 def fizzbuzz(int)
-if int % 3 == 0 
-  return "Fizz"
+if int % 3 == 0 && int % 5 == 0
+  return "FizzBuzz"
 elsif int % 5 == 0
   return "Buzz"
-elseif int % 3 == 0 &&  int % 5 == 0 
+elseif int % 3 == 0 
   return  "FizzBuzz"
   end
 end
